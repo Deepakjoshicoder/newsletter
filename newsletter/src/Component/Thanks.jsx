5b1c6cb9a3797ab5a01b0087a329c1f241644bb6
Deepakjoshicoder <109298702+Deepakjoshicoder@users.noreply.github.com>
2023-06-13 21:58:28 +0530

    firstcommit

--- newsletter/src/Component/Thanks.jsx
+++ newsletter/src/Component/Thanks.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React  from 'react'
+import success from "../assets/images/icon-success.svg"
 
 const Thanks = () => {
+	
+
   return (
-    <div>
-      <h1 className='text-xl text-red-600'>Thanks</h1>
+    <>
+    <div className='bg-[#36384D] h-screen  w-screen flex justify-center items-center'>
+      <div className=' bg-white w-[27%] h-[55%] rounded-3xl  p-10 font-rob'>
+        <img src={success} alt="" className='h-14 w-14 mb-8'/>
+        <p className='text-[hsl(234, 29%, 20%)] leading-10 text-[40px] font-bold mb-7'>Thanks for subscribing!</p>
+        <p className='text-sm mb-8'>A confirmation email has been sent to ash@loremcompany.com. Please open it click the button inside to confirm your subscription.</p>
+        <button className='bg-[#232742] border-b-slate-800 rounded-md h-10 w-[100%] text-white text-sm' x >Dismiss message</button>
+      </div>
+      
     </div>
+    </>
   )
 }
 
