8f3df349c88818184a748a0de839eec7669e96b5
Deepakjoshicoder <109298702+Deepakjoshicoder@users.noreply.github.com>
2023-06-13 22:01:09 +0530

    firstcommit

--- newsletter/src/Component/Thanks.jsx
+++ newsletter/src/Component/Thanks.jsx
@@ -7,7 +7,7 @@ const Thanks = () => {
   return (
     <>
     <div className='bg-[#36384D] h-screen  w-screen flex justify-center items-center'>
-      <div className=' bg-white w-[27%] h-[55%] rounded-3xl  p-10 font-rob'>
+      <div className=' bg-white w-[27%] h-[60%] rounded-3xl  p-10 font-rob'>
         <img src={success} alt="" className='h-14 w-14 mb-8'/>
         <p className='text-[hsl(234, 29%, 20%)] leading-10 text-[40px] font-bold mb-7'>Thanks for subscribing!</p>
         <p className='text-sm mb-8'>A confirmation email has been sent to ash@loremcompany.com. Please open it click the button inside to confirm your subscription.</p>
